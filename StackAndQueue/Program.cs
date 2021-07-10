594762dbadfe41746e64d308fdf85295314ad909
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-07-10 23:29:03 +0530

    [Girish]Creating stack and push the elements into stack

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            Console.WriteLine("Stack and Queue Implementation !");
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+        }
+    }
+}
---
+++ StackAndQueue/Stack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    class Stack
+    {
+        private Node top;
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack",data);
+        }
+
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data+ " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
